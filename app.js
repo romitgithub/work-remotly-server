67f5fbbd236acbbd26f8850e29a4f520388bc654
Romit Choudhary <choudharyromit@gmail.com>
2017-12-19 15:09:26 +0530

    added call for payment gateway

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var paymentController = require('./payment/paymentController');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
