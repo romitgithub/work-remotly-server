fb074326ad6d97c658fee6a4fc929fc89a79ca4d
Romit Choudhary <choudharyromit@gmail.com>
2017-12-19 18:57:28 +0530

    https

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var app = require('./app')
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('key.pem', 'utf8');
+var certificate = fs.readFileSync('cert.pem', 'utf8');
 
-var server = app.listen(port, function(){
-	console.log('Express server listening on port ' + port);
-})
+var credentials = {key: privateKey, cert: certificate};
+
+// your express configuration here
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(port);
+httpsServer.listen(443);
+
+// var server = app.listen(port, function(){
+// 	console.log('Express server listening on port ' + port);
+// })
